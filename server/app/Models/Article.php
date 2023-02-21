0e2b16ec7042c90211abee5da54b75bb88b4ffa2
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2023-02-21 18:43:06 -0300

    models

---
+++ server/app/Models/Article.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Article
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'author',
+        'category',
+        'published_at',
+        'source_id',
+    ];
+
+    /**
+     * Get the source that owns the article.
+     */
+    public function source()
+    {
+        return $this->belongsTo(Source::class);
+    }
+
+}
