e0d9ac804c4838a70f87aa3ef2495d02ad477bc3
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2023-02-23 11:50:03 -0300

    implements JWTSubject

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -69,7 +69,7 @@ class User extends Authenticatable
      */
     public function userSettings()
     {
-        return $this->hasOne(UserSettings::class);
+        return $this->hasOne(UserSetting::class);
     }
 
 }
