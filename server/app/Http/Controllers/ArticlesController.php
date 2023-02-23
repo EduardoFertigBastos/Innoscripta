6a5aa99a0fe0e5232eeaaa93c313d761be8e441a
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2023-02-23 15:50:16 -0300

    Pagination and filter

--- server/app/Http/Controllers/ArticlesController.php
+++ server/app/Http/Controllers/ArticlesController.php
@@ -12,11 +12,25 @@ class ArticlesController extends Controller
     public function index(Request $request)
     {
         try {
-            $data = Article::get();
+            $query = Article::query();
+            $query = $this->buildQueryConditions($query, $request);
+
+            $data = $query->paginate(
+                $request->per_page ?? 20,
+                ['*'],
+                'page',
+                $request->page ?? 1
+            );
 
             return response()->json([
                 'message' => 'Articles returned successfully',
-                'user' => $data
+                'data' => [
+                    'current_page'  => $data->currentPage(),
+                    'last_page'     => $data->lastPage(),
+                    'per_page'      => $data->perPage(),
+                    'total'         => $data->total(),
+                    'articles'      => $data->items(),
+                ]
             ], 201);
         } catch (Exception $ex) {
 
@@ -27,4 +41,24 @@ class ArticlesController extends Controller
         }
     }
 
+    public function buildQueryConditions($query, Request $request)
+    {
+        if ($request->has('fav_categories')) {
+            $query->whereIn('category', $request->fav_categories);
+        }
+
+        if ($request->has('fav_sources')) {
+            $query->whereIn('source', $request->fav_sources);
+        }
+
+        if ($request->has('fav_authors')) {
+            $query->whereIn('author', $request->fav_authors);
+        }
+
+        if ($request->has('from')) {
+            $query->where('published_at', '>=', $request->from);
+        }
+
+        return $query;
+    }
 }
