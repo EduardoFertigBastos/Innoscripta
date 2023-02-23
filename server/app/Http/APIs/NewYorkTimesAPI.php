c99785d37f6ae1cddf86909369f55fd337f68b68
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2023-02-23 09:47:17 -0300

    NewYorkTimesApi

---
+++ server/app/Http/APIs/NewYorkTimesAPI.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\APIs;
+
+use App\Models\Article;
+use App\Helpers\AppHelper;
+use Illuminate\Support\Arr;
+
+class NewYorkTimesAPI extends BaseAPI
+{
+    public $key = 'xe3GGcowzJEAsZhAN6fekG65WDCAr60A';
+    public $paramKey = 'api-key';
+    public $baseUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+
+    public function getArticles()
+    {
+        return $this->data['response']['docs'];
+    }
+
+    public static function createArticle($art)
+    {
+        $art = Arr::dot($art);
+
+        return Article::create([
+            'title'         => AppHelper::getArrayValue($art, 'headline.main'),
+            'description'   => AppHelper::getArrayValue($art, 'abstract'),
+            'author'        => AppHelper::getArrayValue($art, 'byline.original'),
+            'url'           => AppHelper::getArrayValue($art, 'web_url'),
+            'published_at'  => AppHelper::getArrayValue($art, 'pub_date'),
+            'category'      => AppHelper::getArrayValue($art, 'section_name'),
+            'source'        => AppHelper::getArrayValue($art, 'source'),
+        ]);
+    }
+}
