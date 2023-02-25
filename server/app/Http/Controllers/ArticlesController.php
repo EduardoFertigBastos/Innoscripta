4755f9abea6bc912b0569559038f24b6a401abf7
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2023-02-25 09:50:58 -0300

    functions that essentially belong to the model

--- server/app/Http/Controllers/ArticlesController.php
+++ server/app/Http/Controllers/ArticlesController.php
@@ -13,7 +13,7 @@ class ArticlesController extends Controller
     {
         try {
             $query = Article::query();
-            $query = $this->buildQueryConditions($query, $request);
+            $query = Article::buildQueryConditions($query, $request);
 
             $data = $query->paginate(
                 $request->per_page ?? 20,
@@ -41,61 +41,10 @@ class ArticlesController extends Controller
         }
     }
 
-    public function buildQueryCustomizeConditions($query, Request $request)
-    {
-        $query->where(function ($query) use ($request) {
-            if ($request->has('fav_authors')) {
-                $query->orWhereIn('author', $request->fav_authors);
-            }
-
-            if ($request->has('fav_categories')) {
-                $query->orWhereIn('category', $request->fav_categories);
-            }
-
-            if ($request->has('fav_sources')) {
-                $query->orWhereIn('source', $request->fav_sources);
-            }
-        });
-
-        return $query;
-    }
-    public function buildQueryConditions($query, Request $request)
-    {
-        try {
-            $query = $this->buildQueryCustomizeConditions($query, $request);
-            
-            if ($request->has('keyword')) {
-                $query->where(function ($query) use ($request) {
-                    $query->orWhere('title', 'like', '%' . $request->keyword . '%');
-                    $query->orWhere('description', 'like', '%' . $request->keyword . '%');
-                });
-            }
-
-            if ($request->has('category')) {
-                $query->orWhere('category', 'like', '%' . $request->category . '%');
-            }
-
-            if ($request->has('source')) {
-                $query->where('source', 'like', '%' . $request->source . '%');
-            }
-
-            if ($request->has('from')) {
-                $query->where('published_at', '>=', $request->from);
-            }
-
-            return $query;
-        } catch (Exception $ex) {
-            throw new Exception($ex->getMessage());
-        }
-    }
 
     public function authors(Request $request)
     {
-        $authors = Article::groupBy('author')
-            ->orderBy('author')
-            ->whereNotNull('author')
-            ->where('author', '!=', '')
-            ->pluck('author');
+        $authors = Article::authors();
 
         return response()->json([
             'message' => 'Authors returned successfully',
@@ -105,11 +54,7 @@ class ArticlesController extends Controller
 
     public function categories(Request $request)
     {
-        $categories = Article::groupBy('category')
-            ->orderBy('category')
-            ->whereNotNull('category')
-            ->where('category', '!=', '')
-            ->pluck('category');
+        $categories = Article::categories();
 
         return response()->json([
             'message' => 'Categories returned successfully',
@@ -119,11 +64,7 @@ class ArticlesController extends Controller
 
     public function sources(Request $request)
     {
-        $sources = Article::groupBy('source')
-            ->orderBy('source')
-            ->whereNotNull('source')
-            ->where('source', '!=', '')
-            ->pluck('source');
+        $sources = Article::sources();
 
         return response()->json([
             'message' => 'Sources returned successfully',
--- server/app/Models/Article.php
+++ server/app/Models/Article.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Http\Request;
 use Laravel\Sanctum\HasApiTokens;
 
 class Article extends Model
@@ -34,4 +35,79 @@ class Article extends Model
         $this->attributes['category'] = strtolower($value);
     }
 
+    public static function buildQueryCustomizeConditions($query, Request $request)
+    {
+        $query->where(function ($query) use ($request) {
+            if ($request->has('fav_authors')) {
+                $query->orWhereIn('author', $request->fav_authors);
+            }
+
+            if ($request->has('fav_categories')) {
+                $query->orWhereIn('category', $request->fav_categories);
+            }
+
+            if ($request->has('fav_sources')) {
+                $query->orWhereIn('source', $request->fav_sources);
+            }
+        });
+
+        return $query;
+    }
+
+    public static function buildQueryConditions($query, Request $request)
+    {
+        try {
+            $query = Article::buildQueryCustomizeConditions($query, $request);
+
+            if ($request->has('keyword')) {
+                $query->where(function ($query) use ($request) {
+                    $query->orWhere('title', 'ilike', '%' . $request->keyword . '%');
+                    $query->orWhere('description', 'ilike', '%' . $request->keyword . '%');
+                });
+            }
+
+            if ($request->has('category')) {
+                $query->where('category', 'ilike', '%' . $request->category . '%');
+            }
+
+            if ($request->has('source')) {
+                $query->where('source', 'ilike', '%' . $request->source . '%');
+            }
+
+            if ($request->has('from')) {
+                $query->where('published_at', '>=', $request->from);
+            }
+
+            return $query;
+        } catch (Exception $ex) {
+            throw new Exception($ex->getMessage());
+        }
+    }
+
+    public static function authors()
+    {
+        return Article::groupBy('author')
+            ->orderBy('author')
+            ->whereNotNull('author')
+            ->where('author', '!=', '')
+            ->pluck('author');
+    }
+
+    public static function categories()
+    {
+        return Article::groupBy('category')
+            ->orderBy('category')
+            ->whereNotNull('category')
+            ->where('category', '!=', '')
+            ->pluck('category');
+    }
+
+    public static function sources()
+    {
+        return Article::groupBy('source')
+            ->orderBy('source')
+            ->whereNotNull('source')
+            ->where('source', '!=', '')
+            ->pluck('source');
+    }
 }
