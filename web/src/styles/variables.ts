c1ea95b4e1ff1a4b488efbd98a78de70a86d74b2
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2023-02-25 10:52:31 -0300

    Update variables.ts

--- web/src/styles/variables.ts
+++ web/src/styles/variables.ts
@@ -1,5 +1,5 @@
 export const titleColor = '#0a0a0a';
-export const primaryColor = '#ff7878';
+export const primaryColor = '#42b58d';
 export const errorColor = '#f20202';
 export const textColor = '#6C6C80';
 export const successColor = '#4BB543';
