9b9b8aa1ce90eef063756837b0ee242ed1fcd82d
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2023-02-26 23:18:05 -0300

    password validation uppercase and lowercase

--- server/app/Http/Requests/StoreUserRequest.php
+++ server/app/Http/Requests/StoreUserRequest.php
@@ -3,9 +3,28 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Factory as ValidationFactory;
 
 class StoreUserRequest extends FormRequest
 {
+    public function __construct(ValidationFactory $validationFactory)
+    {
+        $validationFactory->extend(
+            'validatePasswordLowercase',
+            function ($attribute, $value, $parameters) {
+                return !!preg_match("#[a-z]+#", $value);
+            },
+            'The password must contain lowercase letters!'
+        );
+
+        $validationFactory->extend(
+            'validatePasswordUppercase',
+            function ($attribute, $value, $parameters) {
+                return !!preg_match("#[A-Z]+#", $value);
+            },
+            'The password must contain uppercase letters!'
+        );
+    }
     /**
      * Determine if the user is authorized to make this request.
      */
@@ -36,6 +55,8 @@ class StoreUserRequest extends FormRequest
                 'required',
                 'string',
                 'min:8',
+                'validatePasswordLowercase',
+                'validatePasswordUppercase',
                 'confirmed'
             ],
         ];
